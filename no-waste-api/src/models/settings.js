ab164062fdd02bf8236cdf87b7c78493991323a3
lucas.feitoza <luckkass89@gmail.com>
2019-11-10 15:05:27 -0300

    testando nova configuração CI/CD

--- no-waste-api/src/models/settings.js
+++ no-waste-api/src/models/settings.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const settingsSchema = mongoose.Schema({
-    institution: {type: String, required: true},
-    averagePeople: {type: Number, required: true},
-    state: {type: {id: Number, sigla: String, nome: String }, required: true },
-    city: {type: {id: Number, nome: String }, required: true }
-  });
-  
-  
-  module.exports = mongoose.model('Settings', settingsSchema);
+  institution: { type: String, required: true },
+  averagePeople: { type: Number, required: true },
+  state: { type: { id: Number, sigla: String, nome: String }, required: true },
+  city: { type: { id: Number, nome: String }, required: true }
+});
+
+
+module.exports = mongoose.model('Settings', settingsSchema);
