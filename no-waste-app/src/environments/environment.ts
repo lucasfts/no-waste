37925ab21653561135a81ef85218b361c063c1fd
Lucas Silva <luckkass89@gmail.com>
2021-09-19 20:54:51 -0300

    Update environment.ts

--- no-waste-app/src/environments/environment.ts
+++ no-waste-app/src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   API_URL: 'http://localhost:3000',
   IBGE_API: 'https://servicodados.ibge.gov.br/api/v1',
   CLIMATEMPO_API: 'http://apiadvisor.climatempo.com.br/api/v1',
-  CLIMATEMPO_TOKEN: 'c17443a416002327771a7df34d8e3c34',
+  CLIMATEMPO_TOKEN: '<CLIMATEMPO_TOKEN>',
 };
 
 /*
